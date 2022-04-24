11dd3f263fe9a85b028f0f8a6c1cbb5266f3ee82
hoskra <hoskra@gmail.com>
2022-04-24 22:51:40 +0200

    crossfade only on jump, ui edit

---
+++ config.js
@@ -0,0 +1,27 @@
+const clock = {
+  filename: "clock_2_0.999_2_0.5__F:37_T:37",
+  folder: "clock",
+  extension: "png",
+  from: 1,
+  to: 37,
+  slice: false
+}
+const cartoon = {
+  filename: "cartoon_3_0.999_2_0.6__F:137_T:137",
+  folder: "cartoon",
+  extension: "bmp",
+  from: 1301,
+  to: 1437,
+  slice: false
+}
+
+const skeleton = {
+  filename: "skeleton_dance_6_0.999_2_0.9__F:125_T:1025",
+  folder: "skeleton_dance",
+  extension: "jpg",
+  from: 950,
+  to: 1020,
+  slice: true
+}
+
+const config = clock;
---
+++ src/script.js
@@ -0,0 +1,201 @@
+function openTab(evt, tabName) {
+  let tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  let tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById('transitions').style.display = "block";
+
+let choice = config;
+
+let TOGGLE = true;
+let RANDOM = false;
+let TINT = false;
+let HUGE_TINT = false;
+let W = 640
+let H = 480
+let f = 0
+let i = 0
+let images_array = []
+let from = choice.from;
+let to = choice.to;
+let frame_cnt = to - from;
+
+let frame_id = document.getElementById('frame');
+let transitions = [];
+let transitions_container = document.getElementById('transitionsContainer')
+let file_name = 'data/' + choice['filename'] + '.txt';
+let img_file_name = 'data/' + choice['filename'] + '.png';
+
+document.getElementById("transitionMatrix").src = img_file_name;
+
+let disabledButtonMessage = "Press stop to enable.";
+let jump = document.getElementById('jump')
+let Linear = document.getElementById("Linear")
+let Random = document.getElementById("Random")
+let None = document.getElementById("None")
+jump.title = disabledButtonMessage;
+
+Linear.onclick = (e=>{
+  TOGGLE = true;
+  RANDOM = false;
+  jump.title = disabledButtonMessage;
+  jump.disabled = true;
+})
+
+Random.onclick = (e=>{
+  TOGGLE = false;
+  RANDOM = true;
+  jump.title = disabledButtonMessage;
+  jump.disabled = true;
+})
+
+None.onclick = (e=>{
+  TOGGLE = false;
+  RANDOM = false;
+  jump.disabled = false;
+  jump.title = "";
+})
+
+let Tint = document.getElementById("Tint")
+let HugeTint = document.getElementById("HugeTint")
+let NoTint = document.getElementById("NoTint")
+
+Tint.onclick = (e=>{ TINT = true; HUGE_TINT = false; })
+HugeTint.onclick = (e=>{ TINT = false; HUGE_TINT = true; })
+NoTint.onclick = (e=>{ TINT = false; HUGE_TINT = false; })
+
+jump.onclick = function() {
+  let len = transitions[i].length;
+  if (len && Math.random() > 0.5) {
+    i = transitions[i][Math.floor(Math.random() * len)];
+  } else {
+    i++;
+  }
+}
+
+function readTextFile(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if(rawFile.readyState === 4) {
+      if(rawFile.status === 200 || rawFile.status == 0) {
+      var allText = rawFile.responseText.split("\n")
+
+      // slicing means not taking all frames from folder
+      if(choice.slice) allText = allText.slice(from, to)
+
+      // loop list of transitions
+      allText.forEach((t,i) => {
+
+        // appending to list
+        let li = document.createElement('li')
+        li.innerHTML = i + ":\t";
+        li.id = i;
+        transitions_container.appendChild(li)
+
+        // loop transitions
+        t = t.slice(1, t.length - 1)
+        if(!t) {
+          transitions.push([])
+        } else {
+          transitions.push(
+
+            (choice.slice) ?
+              t.split(",")
+              .map(x => { return (x >= from && x <= to) ?  x - from : undefined})
+              .filter(x => x !== undefined)
+            :
+              t.split(",")
+              .map(x => { return parseInt(x) })
+            );
+
+            transitions[i].forEach(e => {
+              li.innerHTML += e += " "
+            });
+          }
+        });
+      }
+    }
+  }
+  rawFile.send(null);
+}
+
+readTextFile(file_name);
+
+function preload() {
+  // load images
+  for(let i = from; i < to; i++){
+    let id = i;
+    if (id < 10) { id = '000' + id;
+    } else if (id < 100) { id = '00' + id;
+    } else if (id < 1000) { id = '0' + id; }
+    let image_name = choice['folder'] +"/" + id + "." + choice['extension'];
+    images_array.push(loadImage(image_name));
+  }
+}
+
+function setup() {
+
+  W = images_array[0].width;
+
+  if(W > 400) {
+    W = 400;
+    H = images_array[0].height * 400 / images_array[0].width;
+  } else {
+    H = images_array[0].height > 400 ? 400 : images_array[0].height ;
+  }
+
+  let canvas = createCanvas(W, H);
+  canvas.parent("canvasContainer");
+  document.getElementById("Loading").style.display = "none";
+}
+
+let previousLi = document.getElementById("0")
+function draw() {
+
+  // play video naturally
+  if(TOGGLE && frameCount % 4 == 0) {
+    i++;
+    i %= frame_cnt
+  }
+
+  // jump randomly
+  if(RANDOM && frameCount % 8 == 0) {
+    let len = transitions[i].length;
+    if (len) {
+      if(TINT) {
+        tint(255, 67);
+      } else if(HUGE_TINT) {
+        tint(255, 27);
+      }
+      i = transitions[i][Math.floor(Math.random() * len)];
+    } else {
+      i++;
+    }
+  }
+
+  // color current frame
+  frame_id.innerHTML = i;
+  li = document.getElementById(i);
+  previousLi. style.backgroundColor = 'white';
+  li. style.backgroundColor = 'cyan';
+  previousLi = li;
+
+  // draw image in center
+  push();
+  translate(W/2, H/2);
+  image(images_array[i], -images_array[0].width/2, -images_array[0].height/2);
+  pop();
+}
